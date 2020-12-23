90d8b9dd935f033c07f254026d34af12284c7c8b
arc25275 <arc25275@gmail.com>
2020-12-22 20:25:36 -0800

    Separate events into separate files using a handler

---
+++ events/messageDelete.js
@@ -0,0 +1,40 @@
+const config = require("../config/config.json");
+
+module.exports = {
+  once: false,
+  run(message) {
+    if (message.author.id === message.client.user.id) return;
+    console.log(
+      `Message deleted in #${message.channel.name} has been logged. ${message.cleanContent.length} Chars long. `
+    );
+
+    const embed = {
+      title: "Message Deleted",
+      description: `\`\`\`${message.cleanContent}\`\`\``,
+      color: 13632027,
+      timestamp: `${new Date().toLocaleString()}`,
+      footer: {
+        text: `${message.author.tag}`,
+        icon_url: `${message.author.avatarURL()}`,
+      },
+      thumbnail: {
+        url:
+          "https://media.discordapp.net/attachments/737388909197262948/791077743869296690/trashcan2.png",
+      },
+      fields: [
+        {
+          name: "Message deleted in",
+          value: `<#${message.channel.id}>`,
+          inline: true,
+        },
+        {
+          name: "Message sent by",
+          value: `<@${message.author.id}>`,
+          inline: true,
+        },
+      ],
+    };
+    console.log(embed.description.length);
+    message.client.channels.cache.get(config.logChannel).send({ embed });
+  },
+};
--- main.js
+++ main.js
@@ -1,45 +1,37 @@
+const fs = require("fs");
 const Discord = require("discord.js");
-const client = new Discord.Client();
 const TOKEN = require("./config/auth.json");
 const config = require("./config/config.json");
+
+const client = new Discord.Client();
+client.events = new Discord.Collection();
+
 client.login(TOKEN.token);
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+
+  files.forEach((file) => {
+    const eventFunction = require(`./events/${file}`);
+    if (eventFunction.disabled) return;
+
+    const event = eventFunction.event || file.split(".")[0];
+    const emitter =
+      (typeof eventFunction.emitter === "string"
+        ? client[eventFunction.emitter]
+        : eventFunction.emitter) || client;
+    const once = eventFunction.once;
+
+    try {
+      emitter[once ? "once" : "on"](event, (client) =>
+        eventFunction.run(client)
+      );
+    } catch (error) {
+      console.error(error.stack);
+    }
+  });
+});
 
 client.on("ready", () => {
   console.info(`Logged in as ${client.user.tag}!`);
-});
-
-client.on("messageDelete", async (message) => {
-  if (message.author.id === client.user.id) return;
-  console.log(
-    `Message deleted in #${message.channel.name} has been logged. ${message.cleanContent.length} Chars long. `
-  );
-
-  const embed = {
-    title: "Message Deletion",
-    description: `\`\`\`${message.cleanContent}\`\`\``,
-    color: 13632027,
-    timestamp: `${new Date().toLocaleString()}`,
-    footer: {
-      text: `${message.author.tag}`,
-      icon_url: `${message.author.avatarURL()}`,
-    },
-    thumbnail: {
-      url:
-        "https://media.discordapp.net/attachments/737388909197262948/791077743869296690/trashcan2.png",
-    },
-    fields: [
-      {
-        name: "Message deleted in",
-        value: `<#${message.channel.id}>`,
-        inline: true,
-      },
-      {
-        name: "Message sent by",
-        value: `<@${message.author.id}>`,
-        inline: true,
-      },
-    ],
-  };
-  console.log(embed.description.length);
-  await client.channels.cache.get(config.logChannel).send({ embed });
 });
