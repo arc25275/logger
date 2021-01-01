dd0664f5c79cc732a074c97f7d5eaf264e736e56
arc25275 <arc25275@gmail.com>
2020-12-31 22:29:45 -0800

    Added command to disable logging in a server

---
+++ commands/disable.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+
+module.exports = {
+	name: "disable",
+	description: "Disables Logging",
+	async execute(message, args) {
+		guildID = message.guild.id;
+		newData = {
+			logChannel: "placeholder",
+		};
+		fs.readFile("./config/data.json", (err, data) => {
+			if (err) throw err;
+
+			const jsonData = JSON.parse(data.toString());
+			jsonData[guildID] = newData;
+			fs.writeFile("./config/data.json", JSON.stringify(jsonData), (err) => {
+				if (err) throw err;
+				console.log("The file has been saved!");
+			});
+		});
+		const embed = {
+			description: `Disabled Logging`,
+			color: 13632027,
+		};
+		message.channel.send({ embed });
+	},
+};
