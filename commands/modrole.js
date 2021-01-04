2234394b9db16ec1e7b6962ac9d898ddb47b0470
arc25275 <arc25275@gmail.com>
2021-01-04 10:17:44 -0800

    Added a command to change what roles can run commands

---
+++ commands/modrole.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+const sendError = require("../utils/sendError.js");
+const sendSuccess = require("../utils/sendSuccess.js");
+
+module.exports = {
+	name: "modrole",
+	description: "Adds or removes modroles from the server",
+	async execute(message, args) {
+		const guildID = message.guild.id;
+		const role = message.guild.roles.cache.get(args[1]);
+		fs.readFile("./config/data.json", (err, data) => {
+			if (err) throw err;
+			const jsonData = JSON.parse(data.toString());
+			if (args[0] == "add") {
+				if (jsonData[guildID].modRoles[role.name]) {
+					sendError(message, "Role already a modrole");
+					return;
+				}
+				jsonData[guildID].modRoles[role.name] = args[1];
+				fs.writeFile(
+					"./config/data.json",
+					JSON.stringify(jsonData, null, "\t"),
+					(err) => {
+						if (err) throw err;
+						console.log("The file has been saved! (ModRole added)");
+					}
+				);
+				sendSuccess(message, `ModRole added: ${role.name}`);
+			} else if (args[0] == "remove" || "delete") {
+				if (!jsonData[guildID].modRoles[role.name]) {
+					sendError(message, "Role not a modrole");
+					return;
+				}
+				delete jsonData[guildID].modRoles[role.name];
+				fs.writeFile(
+					"./config/data.json",
+					JSON.stringify(jsonData, null, "\t"),
+					(err) => {
+						if (err) throw err;
+						console.log("The file has been saved! (ModRole Removed)");
+					}
+				);
+				sendError(message, `ModRole Removed: ${role.name}`);
+			}
+		});
+	},
+};
