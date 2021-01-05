098a03334cac4a2d44064e74d9df51b6561eae89
arc25275 <arc25275@gmail.com>
2021-01-05 13:17:55 -0800

    Added modrole errors

--- commands/modrole.js
+++ commands/modrole.js
@@ -7,10 +7,33 @@ module.exports = {
 	description: "Adds or removes modroles from the server",
 	async execute(message, args) {
 		const guildID = message.guild.id;
-		const role = message.guild.roles.cache.get(args[1]);
+
 		fs.readFile("./config/data.json", (err, data) => {
 			if (err) throw err;
+
 			const jsonData = JSON.parse(data.toString());
+			var role;
+			if (!args[0]) {
+				sendError(message, "Invalid Option (must be either `add` or `remove`)");
+				return;
+			}
+			if (!args[1]) {
+				sendError(message, "Invalid Role provided");
+				return;
+			}
+			if (args[1].match(/^\d{18}/)) {
+				role = message.guild.roles.cache.get(args[1]);
+			} else if (args[1].match(/^<@&\d{18}>$/)) {
+				role = message.guild.roles.cache.get(
+					args[1].replace("<@&", "").replace(">", "")
+				);
+			} else {
+				role = message.guild.roles.cache.find((role) => role.name === args[1]);
+			}
+			if (!role) {
+				sendError(message, "Invalid Role provided");
+				return;
+			}
 			if (args[0] == "add") {
 				if (jsonData[guildID].modRoles[role.name]) {
 					sendError(message, "Role already a modrole");
@@ -25,7 +48,7 @@ module.exports = {
 						console.log("The file has been saved! (ModRole added)");
 					}
 				);
-				sendSuccess(message, `ModRole added: ${role.name}`);
+				sendSuccess(message, `ModRole added: \`${role.name}\``);
 			} else if (args[0] == "remove" || "delete") {
 				if (!jsonData[guildID].modRoles[role.name]) {
 					sendError(message, "Role not a modrole");
@@ -40,7 +63,7 @@ module.exports = {
 						console.log("The file has been saved! (ModRole Removed)");
 					}
 				);
-				sendError(message, `ModRole Removed: ${role.name}`);
+				sendError(message, `ModRole removed: \`${role.name}\``);
 			}
 		});
 	},
